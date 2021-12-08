fixed judgement calculation bug

Users.py
@@ -27,14 +27,6 @@ class User():
         self.testUtterence = self.utterences[-1]
         self.utterences.pop()
 
-    
-    # def splitToPairs(self,pairs=61,words=2):
-    #     self.pairs = []
-    #     for i in range(pairs):
-    #         wordsList = []
-    #         for j in range(words):
-    #             wordsList.append(self.utterences[i*words+j])
-    #         self.pairs.append(wordsList)
 
     def getJudgements(self,references,threshold=np.inf):
         judgements = np.zeros((len(self.utterences),5))
@@ -44,11 +36,11 @@ class User():
             d12 = self.utterences[i*2].distance(references[self.reference].utterences[i*2+1])
             d21 = self.utterences[i*2+1].distance(references[self.reference].utterences[i*2])
             d22 = self.utterences[i*2+1].distance(references[self.reference].utterences[i*2+1])
-            if d11<=d12 and d11<=d21 and d11<=d22 and d11<=threshold:
+            if d11<=d12 and d11<=threshold:
                 judgements[i*2][2] += 1
                 judgements[i*2][3] += 1
                 self.utterences[i*2].correct = True
-            elif d12<=d11 and d12<=d21 and d12<=d22 and d12<=threshold:
+            elif d12<=d11 and d12<=threshold:
                 judgements[i*2][1] += 1
                 judgements[i*2][4] += 1
                 self.utterences[i*2].correct = False
@@ -56,14 +48,14 @@ class User():
                 judgements[i*2][0] += 1
                 judgements[i*2][4] += 1
                 self.utterences[i*2].correct = False
-            if d22<=d11 and d22<=d12 and d22<=d21 and d22<=threshold:
+            if d22<=d21 and d22<=threshold:
                 judgements[i*2+1][1] += 1
                 judgements[i*2+1][3] += 1
-                self.utterences[i*2+1].correct = False
-            elif d21<=d11 and d21<=d12 and d21<=d22 and d12<=threshold:
+                self.utterences[i*2+1].correct = True
+            elif d21<=d22 and d21<=threshold:
                 judgements[i*2+1][2] += 1
                 judgements[i*2+1][4] += 1
-                self.utterences[i*2+1].correct = True
+                self.utterences[i*2+1].correct = False
             else:
                 judgements[i*2+1][0] += 1
                 judgements[i*2+1][4] += 1

calculateJudgements.py
@@ -13,12 +13,12 @@ users = loadUser('users')
 for reference in references:
     reference.setTestUtterence()
 
-#
+# for all users set the test reference to utterence number 123 and calculate the reference user
 for user in users:
     user.setTestUtterence()
     user.calculateReference(references)
 
-
+# calculate the judgements for a set of users
 def calculateJudgements(users, references ,print_results=False):
     usersSize = len(users)
     print('usersSize: ', usersSize)
@@ -81,7 +81,10 @@ sources = [
     'M',
     'W'
 ]
+
 def drawMismatches(users,numberOfMismatches=5, plot_results=False,save_results=False):
+    plt.xlabel("frame")
+    plt.ylabel("mismatch")
     for user in users:
         name = "G{}S{}{}{}{}".format(user.group,user.student,types[user.Type],user.age,sources[user.source])
         rows = int(np.sqrt(numberOfMismatches))
@@ -89,19 +92,17 @@ def drawMismatches(users,numberOfMismatches=5, plot_results=False,save_results=F
         j = 0
         for i,utterence in enumerate(user.utterences):
             _, _, distances = utterence.reconstruct(references[user.reference].utterences[i])
-            if distances is not None:
+            if distances is not None and not user.utterences[i].correct:
                 utterenceDetails = "W{}P{}".format(utterence.pair,utterence.word)
-                plt.xlabel("frame")
-                plt.ylabel("mismatch")
-                plt.subplot(rows,columns,i+1,title=utterenceDetails)
+                plt.subplot(rows,columns,j+1,title=name+utterenceDetails)
                 plt.plot(distances)
                 j += 1
             if j == numberOfMismatches:
                 break
-        print("saving figure: ", name)
         if save_results:
+            print("saving figure: ", name)
             plt.savefig("plots/"+name+".png")
         if plot_results:
             plt.show()
 
-drawMismatches(users,numberOfMismatches=5, plot_results=True,save_results=True)
+drawMismatches(users,numberOfMismatches=11, plot_results=False,save_results=True)