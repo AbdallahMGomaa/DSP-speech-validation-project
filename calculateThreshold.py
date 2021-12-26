71402bdf9a473e02dcfb398b74327e50fd3024b4
Abdal <abdallah.mamdouh92@icloud.com>
2021-12-27 01:27:18 +0200

    calculated threshold as the max of all distances

--- calculateThreshold.py
+++ calculateThreshold.py
@@ -1,15 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def calculateThreshold(users,references,size=20,words=122):
-    # d = np.zeros((size*words))
-    # # size = len(users)
-    # k = 0
-    # for i in range(words//2):
-    #     for j in range(size):
-    #         d[k] = users[j].utterences[2*i].distance(references[users[j].reference].utterences[2*i])
-    #         d[k+1] = users[j].utterences[2*i+1].distance(references[users[j].reference].utterences[2*i+1])
-    #         k = k+2
-    # d = d[d!=np.inf]
-    # threshold = np.mean(d)/0.75
-    threshold = np.inf
+def calculateThreshold(users,references,words=122):
+    size = len(users)
+    d = np.zeros((size*words))
+    k = 0
+    for i in range(words//2):
+        for j in range(size):
+            d[k] = users[j].utterences[2*i].distance(references[users[j].reference].utterences[2*i])
+            d[k+1] = users[j].utterences[2*i+1].distance(references[users[j].reference].utterences[2*i+1])
+            k = k+2
+    d = d[d!=np.inf]
+    threshold = max(d)
     return threshold
