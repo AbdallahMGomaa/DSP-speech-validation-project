ce10ed5e03576842e2692eb32886c6025712cb40
Abdal <abdallah.mamdouh92@icloud.com>
2021-12-30 14:45:56 +0200

    changed threshold calculation algorithm

--- calculateThreshold.py
+++ calculateThreshold.py
@@ -3,15 +3,11 @@ def calculateThreshold(users,references,words=122):
     refSize = len(references)
     userSize = len(users)
     thresholds = np.zeros((words))
-    d = np.zeros((userSize*(words-1)*refSize))
-    for i in range(words):
-        m = 0
-        for j in range(userSize):
-            for l in range(refSize):
-                for k in range(words):
-                    if k != i:
-                        d[m] = users[j].utterences[i].distance(references[l].utterences[k])
-                        m += 1
-        d = d[d!=np.inf]
-        thresholds[i] = np.mean(d)
+    for i in range(userSize):
+        for j in range(words//2):
+            d11 = users[i].utterences[j*2].distance(references[users[i].reference].utterences[j*2])
+            d22 = users[i].utterences[j*2+1].distance(references[users[i].reference].utterences[j*2+1])
+    d11 = d11[d11!=np.inf]
+    d22 = d22[d22!=np.inf]
+    thresholds[i] = (np.mean(d11)+np.mean(d22))*0.6
     return thresholds
